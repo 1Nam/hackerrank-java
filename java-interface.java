a9809ee0bb63e11a984e7044d1b0e0ca5cc50bd9
1Nam <namnh.ftu@gmail.com>
2017-05-27 23:11:10 +0700

    Create java-interface.java

---
+++ java-interface.java
@@ -0,0 +1,13 @@
+//Write your code here
+class MyCalculator implements AdvancedArithmetic {
+    public int divisor_sum(int n) {
+        int sum = 0;
+        for (int i = 1; i < n / 2 + 1; i++){
+            if ((n % i) == 0){
+                sum = sum + i;
+            }
+        }
+        sum = sum + n;
+        return sum;
+    }
+}
