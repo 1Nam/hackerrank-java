715f930a15b15c4023b6a7d84116792c29696398
1Nam <namnh.ftu@gmail.com>
2017-05-27 23:16:42 +0700

    Create java-factory.java

---
+++ java-factory.java
@@ -0,0 +1,8 @@
+
+        if(order.equalsIgnoreCase("Pizza")) {
+            return new Pizza();
+        } else if (order.equalsIgnoreCase("Cake")) {
+            return new Cake();
+        } else {
+            return null;
+        }
