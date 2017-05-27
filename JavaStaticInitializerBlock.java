44d175bf37895c09c15d6c0261af7e43769fdeb3
1Nam <namnh.ftu@gmail.com>
2017-05-27 22:46:51 +0700

    Update JavaStaticInitializerBlock.java

--- JavaStaticInitializerBlock.java
+++ JavaStaticInitializerBlock.java
@@ -8,18 +8,18 @@ public class Solution {
 
 //Write your code here
 
-static int B;
-static int H;
-static boolean flag = true;
-static {
-    Scanner sc = new Scanner(System.in);
-    B = sc.nextInt();
-    H = sc.nextInt();
-    if(B<=0 || H<=0) {
-        System.out.println("java.lang.Exception: Breadth and height must be positive");
-        flag = false;
-    }
-}
+	static int B;
+	static int H;
+	static boolean flag = true;
+	static {
+	    Scanner sc = new Scanner(System.in);
+	    B = sc.nextInt();
+	    H = sc.nextInt();
+	    if(B<=0 || H<=0) {
+		System.out.println("java.lang.Exception: Breadth and height must be positive");
+		flag = false;
+	    }
+	}
 
 public static void main(String[] args){
 		if(flag){
